9a6a482695bcb7af4e5dece1d5c6b9ffce6ba70c
Denis Kosogov <denis.kosogov@gmail.com>
2024-04-05 15:34:13 -0400

    Update SponsorsTemplate.tsx

--- frontend/src/components/templates/SponsorsTemplate.tsx
+++ frontend/src/components/templates/SponsorsTemplate.tsx
@@ -17,19 +17,19 @@ export default function SponsorsTemplate({ content }: { content: any }) {
         {content.template[0].sponsor.length === 0 && (
           <div>Please upload some content!</div>
         )}
-        {content.template[0].sponsor.map((person: any, index: number) => (
+        {content.template[0].sponsor.map((sponsor: any, index: number) => (
           <div className="relative rounded-lg overflow-hidden shadow-lg group">
             <Image
-              src={getStrapiMedia(person.media.data.attributes.url) || "/"}
-              width={person.media.data.attributes.width}
-              height={person.media.data.attributes.height}
-              alt={person.name}
+              src={getStrapiMedia(sponsor.media.data.attributes.url) || "/"}
+              width={sponsor.media.data.attributes.width}
+              height={sponsor.media.data.attributes.height}
+              alt={sponsor.name}
               className="w-full object-contain transition-transform group-hover:scale-105 min-h-[200px]"
               style={{ height: "100%" }}
             />
             <div className="absolute bottom-0 left-0 right-0 bg-black bg-opacity-70 p-4 transition-opacity duration-300 group-hover:bg-opacity-80">
-              <p className="text-white text-lg font-medium">{person.name}</p>
-              <p className="text-gray-300 text-sm">{person.title}</p>
+              <p className="text-white text-lg font-medium">{sponsor.name}</p>
+              <p className="text-gray-300 text-sm">{sponsor.description}</p>
             </div>
           </div>
         ))}
