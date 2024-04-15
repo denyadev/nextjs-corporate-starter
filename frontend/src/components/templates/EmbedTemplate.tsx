9f770ba2e27171917ad52781efe79291ffd447bb
Denis Kosogov <denis.kosogov@gmail.com>
2024-04-15 14:23:28 -0400

    Update EmbedTemplate.tsx

--- frontend/src/components/templates/EmbedTemplate.tsx
+++ frontend/src/components/templates/EmbedTemplate.tsx
@@ -5,9 +5,8 @@ export default function EmbedTemplate({ content }: { content: any }) {
         src={content.url}
         style={{ width: "100%", height: "90vh", border: "none" }}
         title={content.heading}
-        name="htmlComp-iframe"
-        scrolling="auto"
-        sandbox="allow-same-origin allow-forms allow-popups allow-scripts allow-pointer-lock"
+        // name="htmlComp-iframe"
+        // sandbox="allow-same-origin allow-forms allow-popups allow-scripts allow-pointer-lock"
       />
     </div>
   );
