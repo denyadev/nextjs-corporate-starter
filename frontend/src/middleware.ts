cdfbffccd900526d144338bfde350f33b6989cc1
Denis Kosogov <denis.kosogov@gmail.com>
2024-05-30 16:18:13 -0400

    Update middleware.ts

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -9,18 +9,23 @@ export function middleware(request: NextRequest) {
   // Extract the URL pathname
   const { pathname } = request.nextUrl;
 
-  // Check if the pathname matches the pattern /[slug]
-  const slugPattern = /^\/([^\/]+)$/;
+  // Check if the pathname matches the pattern /[slug] or /fr/[slug]
+  const slugPattern = /^\/(fr\/)?([^\/]+)$/;
   const match = pathname.match(slugPattern);
+
   if (match) {
-    // Redirect to /[slug]/agenda
-    const slug = match[1];
-    return NextResponse.redirect(new URL(`/${slug}/agenda`, request.url));
+    // Redirect to /[slug]/agenda or /fr/[slug]/agenda
+    const prefix = match[1] || "";
+    const slug = match[2];
+    return NextResponse.redirect(
+      new URL(`/${prefix}${slug}/agenda`, request.url)
+    );
   }
 
   // Return the i18n response if no redirection is needed
   return response;
 }
+
 export const config = {
   matcher: "/((?!api|static|.*\\..*|_next).*)",
 };
