527323a149690194eafb33bda0fdb8cba003299b
Denis Kosogov <denis.kosogov@gmail.com>
2024-06-06 12:42:06 -0400

    update

--- frontend/src/components/MeetingComponent.tsx
+++ frontend/src/components/MeetingComponent.tsx
@@ -1,33 +1,55 @@
+// "use client";
+
+// import React, { useEffect, useState } from "react";
+// import { Button } from "@/components/ui/button";
+// import { Input } from "@/components/ui/input";
+// import { useZoom } from "@/context/ZoomContext";
+
+// const Meeting: React.FC = () => {
+//   const { initializeZoom, zoomContainerRef, isInitialized } = useZoom();
+//   const [userName, setUserName] = useState<string>("User");
+
+//   const handleJoinClick = () => {
+//     if (!isInitialized && userName) {
+//       initializeZoom("86322258133", userName);
+//     }
+//   };
+
+//   return (
+//     <div className="relative p-0 m-0 w-full max-w-screen flex flex-col items-center justify-center">
+//       <Button>Zoom</Button>
+//       <div className="flex gap-2">
+//         <Input
+//           placeholder="Name"
+//           value={userName}
+//           onChange={(e) => setUserName(e.target.value)}
+//         />
+//         <Button onClick={handleJoinClick} variant="outline">
+//           Join Meeting
+//         </Button>
+//       </div>
+//       <div ref={zoomContainerRef} id="meetingSDKElement"></div>
+//     </div>
+//   );
+// };
+
+// Meeting.displayName = "Zoom Component View";
+
+// export default Meeting;
+
 "use client";
 
-import React, { useState } from "react";
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
 import { useZoom } from "@/context/ZoomContext";
+import React, { useEffect } from "react";
 
 const Meeting: React.FC = () => {
-  const { initializeZoom, zoomContainerRef, isInitialized } = useZoom();
-  const [userName, setUserName] = useState<string>("");
+  const { zoomContainerRef, setMeetingNumber } = useZoom();
 
-  const handleJoinClick = () => {
-    if (!isInitialized && userName) {
-      initializeZoom("86322258133", userName);
-    }
-  };
-
+  useEffect(() => {
+    setMeetingNumber("86322258133");
+  }, [setMeetingNumber]);
   return (
     <div className="relative p-0 m-0 w-full max-w-screen flex flex-col items-center justify-center">
-      <Button>Zoom</Button>
-      <div className="flex gap-2">
-        <Input
-          placeholder="Name"
-          value={userName}
-          onChange={(e) => setUserName(e.target.value)}
-        />
-        <Button onClick={handleJoinClick} variant="outline">
-          Join Meeting
-        </Button>
-      </div>
       <div ref={zoomContainerRef} id="meetingSDKElement"></div>
     </div>
   );
--- frontend/src/context/ZoomContext.tsx
+++ frontend/src/context/ZoomContext.tsx
@@ -1,4 +1,93 @@
 "use client";
+// // src/contexts/ZoomContext.tsx
+// import React, {
+//   createContext,
+//   useContext,
+//   useState,
+//   ReactNode,
+//   useRef,
+//   useEffect,
+// } from "react";
+// import { generateSignature } from "@/lib/generateSignature";
+
+// interface ZoomContextProps {
+//   meetingNumber: string | null;
+//   setMeetingNumber: (meetingNumber: string | null) => void;
+//   initializeZoom: (meetingNumber: string, userName: string) => void;
+//   zoomContainerRef: React.RefObject<HTMLDivElement>;
+//   isInitialized: boolean;
+//   setIsInitialized: (isInitialized: boolean) => void;
+// }
+
+// const ZoomContext = createContext<ZoomContextProps | undefined>(undefined);
+
+// export const useZoom = (): ZoomContextProps => {
+//   const context = useContext(ZoomContext);
+//   if (!context) {
+//     throw new Error("useZoom must be used within a ZoomProvider");
+//   }
+//   return context;
+// };
+
+// interface ZoomProviderProps {
+//   children: ReactNode;
+// }
+
+// export const ZoomProvider: React.FC<ZoomProviderProps> = ({ children }) => {
+//   const [meetingNumber, setMeetingNumber] = useState<string | null>(null);
+//   const [isInitialized, setIsInitialized] = useState<boolean>(false);
+//   const zoomContainerRef = useRef<HTMLDivElement>(null);
+//   const clientRef = useRef<any>(null);
+
+//   const initializeZoom = async (meetingNumber: string, userName: string) => {
+//     if (!zoomContainerRef.current || isInitialized) return;
+
+//     try {
+//       const ZoomEmbed = (await import("@zoomus/websdk/embedded")).default;
+//       const client = ZoomEmbed.createClient();
+//       const signature = generateSignature(meetingNumber);
+
+//       client.init({
+//         language: "en-US",
+//         zoomAppRoot: zoomContainerRef.current,
+//       });
+//       console.log(meetingNumber, userName);
+//       client.join({
+//         sdkKey: process.env.NEXT_PUBLIC_ZOOM_CLIENT_ID!,
+//         signature: signature,
+//         meetingNumber,
+//         userName,
+//       });
+
+//       clientRef.current = client;
+//       setIsInitialized(true);
+//     } catch (error) {
+//       console.error("Error during Zoom meeting initialization:", error);
+//     }
+//   };
+
+//   useEffect(() => {
+//     if (meetingNumber) {
+//       initializeZoom(meetingNumber, "Default User");
+//     }
+//   }, [meetingNumber]);
+
+//   return (
+//     <ZoomContext.Provider
+//       value={{
+//         meetingNumber,
+//         setMeetingNumber,
+//         initializeZoom,
+//         zoomContainerRef,
+//         isInitialized,
+//         setIsInitialized,
+//       }}
+//     >
+//       {children}
+//     </ZoomContext.Provider>
+//   );
+// };
+
 // src/contexts/ZoomContext.tsx
 import React, {
   createContext,
@@ -51,7 +140,7 @@ export const ZoomProvider: React.FC<ZoomProviderProps> = ({ children }) => {
         language: "en-US",
         zoomAppRoot: zoomContainerRef.current,
       });
-      console.log(meetingNumber, userName);
+
       client.join({
         sdkKey: process.env.NEXT_PUBLIC_ZOOM_CLIENT_ID!,
         signature: signature,
@@ -67,10 +156,10 @@ export const ZoomProvider: React.FC<ZoomProviderProps> = ({ children }) => {
   };
 
   useEffect(() => {
-    if (meetingNumber) {
-      initializeZoom(meetingNumber, "Default User");
+    if (meetingNumber && !isInitialized) {
+      initializeZoom(meetingNumber, "Test");
     }
-  }, [meetingNumber]);
+  }, [meetingNumber, isInitialized]);
 
   return (
     <ZoomContext.Provider
